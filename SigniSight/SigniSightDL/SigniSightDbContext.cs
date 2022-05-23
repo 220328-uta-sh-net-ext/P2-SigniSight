21ed16d6c212ef8527c59cafc3f168ad523a41be
EdithKennedyTynes <edith.tynes18@gmail.com>
2022-05-23 16:28:04 -0400

    DL update

---
+++ SigniSight/SigniSightDL/SigniSightDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigniSightDL
+{
+    public class SigniSightDbContext //: DBContext like referenece to the database responsibile to create connection with db,
+    {
+    }
+}
