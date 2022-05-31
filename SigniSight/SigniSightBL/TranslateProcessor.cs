0824d8885f7fb265beac7975f957081b3ed48026
Daniel Oszczapinski <dannyoszczapinski@gmail.com>
2022-05-31 11:59:38 -0500

    pipepline changes

--- SigniSight/SigniSightBL/TranslateProcessor.cs
+++ SigniSight/SigniSightBL/TranslateProcessor.cs
@@ -1,16 +1,17 @@
-﻿using System.Text;
+﻿
+using System.Text;
 using Newtonsoft.Json;
 
 namespace SigniSightBL
 {
     public class TranslateProcessor
     {
-        static string subscriptionKeyFilePath = "SigniSightBL/TranslateKey.txt";
+        static string subscriptionKeyFilePath = "../SigniSightBL/TranslateKey.txt";
         static string key = System.IO.File.ReadAllText(subscriptionKeyFilePath);
-        static string endpointFilePath = "SigniSightBL/TranslateLocation.txt";
+        static string endpointFilePath = "../SigniSightBL/TranslateLocation.txt";
         static string location = System.IO.File.ReadAllText(endpointFilePath);
         private static readonly string endpointTranslate = "https://api.cognitive.microsofttranslator.com/";
-       // private static string route = "/translate?api-version=3.0&to=en"; //change it to variable
+        // private static string route = "/translate?api-version=3.0&to=en"; //change it to variable
         public static async Task<string> TranslateText(string textToTranslate, string language)
         {
             string route = $"/translate?api-version=3.0&to={language}";
